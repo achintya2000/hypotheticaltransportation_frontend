b686423190f0ce70f171fcbf21ed23154b7c0cfc
Achintya Kumar <ask60@duke.edu>
2022-01-16 17:33:12 -0500

    initial ui

---
+++ src/components/NavBar.vue
@@ -0,0 +1,85 @@
+<template>
+  <nav>
+    <v-app-bar color="#FFFFFF" flat app>
+      <v-app-bar-nav-icon
+        class="hidden-md-and-up"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+
+      <v-spacer class="hidden-md-and-up"></v-spacer>
+
+      <router-link to="/" style="text-decoration: none; color: inherit">
+        <v-toolbar-title> Hypothetical Transportation </v-toolbar-title>
+      </router-link>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        class="hidden-sm-and-down"
+        text
+        v-for="name in names"
+        :key="name.title"
+      >
+        <router-link
+          :to="name.to"
+          style="text-decoration: none; color: inherit"
+          >{{ name.title }}</router-link
+        >
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app class="hidden-md-and-up">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">Application</v-list-item-title>
+          <v-list-item-subtitle>subtext</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="name in names" :key="name.title" link :to="name.to">
+          <v-list-item-content>
+            <v-list-item-title>{{ name.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item
+          v-for="contact in contactpoints"
+          :key="contact.title"
+          link
+          :href="contact.link"
+          target="_blank"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ contact.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ contact.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      names: [{ title: "Login", to: "/login" }],
+      contactpoints: [],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <nav-bar />
+    <hello-world />
+  </v-container>
+</template>
+
+<script>
+import HelloWorld from "../components/HelloWorld";
+import NavBar from "../components/NavBar.vue";
+
+export default {
+  name: "Home",
+
+  components: {
+    HelloWorld,
+    NavBar,
+  },
+};
+</script>
