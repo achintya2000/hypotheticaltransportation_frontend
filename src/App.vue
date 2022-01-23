a059853a314a854ada2d8a7a667d8df6dc1bba2f
lubenjamin <lubenjamin@gmail.com>
2022-01-23 18:25:10 -0500

    api testing

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,15 @@
       <router-view />
     </v-main>
   </v-app>
+
+  <div>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
 </template>
+
 
 <script>
 import NavBar from "./components/NavBar.vue";
@@ -20,5 +28,29 @@ export default {
   data: () => ({
     //
   }),
+};
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        const response = await this.$http.get(
+          "http://jsonplaceholder.typicode.com/posts"
+        );
+        // JSON responses are automatically parsed.
+        this.posts = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
