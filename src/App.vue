cad8b298fb02bedbfa0e7f112fc37f2a4bb35f63
Achintya Kumar <ask60@duke.edu>
2022-01-23 19:32:58 -0500

    fixed navbar

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,7 @@
       <router-view />
     </v-main>
   </v-app>
-
-  <div>
-    <div v-for="post in posts" v-bind:key="post.id">
-      <h2>{{ post.title }}</h2>
-      <p>{{ post.body }}</p>
-    </div>
-  </div>
 </template>
-
 
 <script>
 import NavBar from "./components/NavBar.vue";
@@ -28,29 +20,5 @@ export default {
   data: () => ({
     //
   }),
-};
-</script>
-
-<script>
-export default {
-  data() {
-    return {
-      posts: [],
-    };
-  },
-
-  methods: {
-    async getData() {
-      try {
-        const response = await this.$http.get(
-          "http://vcm-24682.vm.duke.edu:8000/api/school/get/1"
-        );
-        // JSON responses are automatically parsed.
-        this.posts = response.data;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  },
 };
 </script>
