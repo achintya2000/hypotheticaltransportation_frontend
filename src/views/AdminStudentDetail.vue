3e6979e40524a6f7ade0b60fa977cc8bdb6915d2
Michael Dodd <mmd61@duke.edu>
2022-01-25 13:49:46 -0500

    Added stuff

--- src/views/AdminStudentDetail.vue
+++ src/views/AdminStudentDetail.vue
@@ -3,7 +3,83 @@
     <v-card-title>
       Jane Doe
       <v-spacer></v-spacer>
-      <v-btn outlined>Modify</v-btn>
+      <v-dialog v-model="dialog2" width="500">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
+            Modify
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2">
+            Modify
+          </v-card-title>
+
+          <v-card-text>
+            <v-form
+              ref="form"
+              v-model="valid"
+              lazy-validation
+            >
+              <v-text-field
+                v-model="name"
+                :rules="nameRules"
+                label="Name"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="studentID"
+                :rules="studentIDRules"
+                label="Student ID"
+                required
+              ></v-text-field>
+
+              <v-autocomplete
+                v-model="parentValue"
+                :items="parentItems"
+                label="Parent"
+              ></v-autocomplete>
+
+              <v-autocomplete
+                v-model="schoolValue"
+                :items="schoolItems"
+                label="School"
+              ></v-autocomplete>
+              
+              <v-autocomplete
+                v-model="busRouteValue"
+                :items="busRouteItems"
+                label="Bus Route"
+              ></v-autocomplete>
+
+              <v-btn
+                :disabled="!valid"
+                color="success"
+                class="mr-4"
+                @click="validate"
+              >
+                Submit
+              </v-btn>
+
+              <v-btn
+                color="error"
+                class="mr-4"
+                @click="reset"
+              >
+               Clear
+              </v-btn>
+
+              <v-btn
+                color="warning"
+                @click="reset"
+              >
+                Cancel
+              </v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
       <v-spacer></v-spacer>
       <v-dialog v-model="dialog" width="500">
         <template v-slot:activator="{ on, attrs }">
@@ -33,7 +109,7 @@
 
               <v-btn
                 color="success"
-                @click="reset"
+                @click="dialog = false"
               >
                 Cancel
               </v-btn>
@@ -59,8 +135,50 @@
 export default {
   data() {
     return {
+        dialog: false,
+        dialog2: false,
+        name: 'Old Name',
+        nameRules: [
+            v => !!v || 'Name is required',
+        ],
+        studentID: 'Old Student ID',
+        studentIDRules: [
+            v => !!v || 'Student ID is required',
+        ],
+        parentValue: 'foo',
+        parentRules: [
+            v => !!v || 'Parent is required',
+        ],
+        schoolValue: 'Old School',
+        schoolRules: [
+            v => !!v || 'School is required',
+        ],
+        busRouteValue: 'Old Bus Route',
+        busRouteRules: [
+            v => !!v || 'Bus Route is required',
+        ],
+        parentItems: ['foo', 'bar', 'fizz', 'buzz'],
+        parentValues: ['foo', 'bar'],
+        parentValue: null,
+        schoolItems: ['foo', 'bar', 'fizz', 'buzz'],
+        schoolValues: ['foo', 'bar'],
+        schoolValue: null,
+        busRouteItems: ['foo', 'bar', 'fizz', 'buzz'],
+        busRouteValues: ['foo', 'bar'],
+        busRouteValue: null,
     };
   },
+  methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
 };
 </script>
 
---
+++ src/views/AdminUserCreate.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Create a New User
+    </v-card-title>
+    <v-card-text>
+
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<style>
+</style>
