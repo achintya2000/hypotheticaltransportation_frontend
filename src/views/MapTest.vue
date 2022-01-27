7ca67195f8be125b1a46534c112c6ce86434de67
Achintya Kumar <ask60@duke.edu>
2022-01-27 00:35:44 -0500

    map test works

---
+++ src/views/MapTest.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <h2>Search and add a pin</h2>
+    </div>
+    <br />
+    <GmapMap :center="center" :zoom="12" style="width: 100%; height: 400px" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MapTest",
+  data() {
+    return {
+      center: { lat: 45.508, lng: -73.587 },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
