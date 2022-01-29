f1ec212f3faa1f9e735baf84a0dff7fc8222a6c9
Achintya Kumar <ask60@duke.edu>
2022-01-29 17:20:17 -0500

    view and data loading works

--- src/services/axios-api.js
+++ src/services/axios-api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const base_endpoint = axios.create({
     baseURL: 'https://vcm-24682.vm.duke.edu:8000',
-    timeout: 1000,
+    timeout: 5000,
 })
 
 export { base_endpoint }
